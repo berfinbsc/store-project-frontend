b642a75a0a60260ee1a06c2a2e73aa17a4d9a7f1
berfin7 <berfinbasci4701@gmail.com>
2024-02-09 03:17:06 +0300

    update

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -6,13 +6,19 @@ function ProductComponent() {
 
 
       
-      <div className="ui  card">
+      <div className="ui card">
 
       <div className="image content">
-      <img src='../images/p.png' alt="img" height={300} width={300}/>
+      <img src='../images/p.png' alt="img" height={350} width={400}/>
       </div>
 
+
+
       <div className="content">
+      <div >
+        <span>edeı</span>
+      </div>
+
       <span className="right floated">
         <i className="heart outline like icon"></i>
         17 likes
